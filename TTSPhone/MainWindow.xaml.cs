c758070421059915b4bb0db8173022b7aec408e2
Anthony Brown <bruinbrown@gmail.com>
2012-06-02 17:07:45 +0100

    Added a lot of networking functionality

---
+++ TTSPhone/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace TTSPhone
+{
+    public partial class MainWindow : Window
+    {
+        public static Thread listenThread;
+        Network network;
+        public MainWindow()
+        {
+            InitializeComponent();
+            network = new Network();
+            network.StartListening(3301);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            network.TryConnect("localhost");
+            network.Send(Encoding.ASCII.GetBytes("HELLO"));
+        }
+    }
+}
---
+++ TTSPhone/Network.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace TTSPhone
+{
+    class Network
+    {
+        TcpListener listener;
+        TcpClient client;
+        NetworkStream stream;
+        byte[] receivedByes;
+
+        public Network()
+        {
+            receivedByes = new byte[4096];
+        }
+
+        public void StartListening(int port)
+        {
+            listener = new TcpListener(port);
+            MainWindow.listenThread = new System.Threading.Thread(this.Listen);
+        }
+
+        private void Listen()
+        {
+            listener.Start();
+            if (listener.Pending())
+            {
+                MessageBoxResult result = MessageBox.Show("Incoming call from     Accept", "Incoming call", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.No)
+                {
+                    this.listener.Server.Disconnect(true);
+                }
+                else if (result == MessageBoxResult.Yes)
+                {
+                    client = listener.AcceptTcpClient();
+                    stream = client.GetStream();
+                }
+            }
+        }
+
+        public void Send(byte[] buffer)
+        {
+        }
+
+        public void TryConnect(string IPAddress)
+        {
+            try
+            {
+                client = new TcpClient(IPAddress, 3301);
+                stream = client.GetStream();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occurred: " + ex.Message.ToString());
+            }
+        }            
+    }
+}
